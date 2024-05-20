ccdcf591dbd9ed7e01523830d4f78238c2b00b5b
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-05-20 16:44:02 -0400

    modify cell size feature

--- main.cpp
+++ main.cpp
@@ -14,15 +14,16 @@ using namespace sf;
 int main()
 {
     const Vector2i windowSize = {800, 500};
-    const int cellSize = 10;
-    const Vector2i gridSize = {windowSize.x/cellSize, windowSize.y/cellSize};
+    const vector<int> cellSizes = {4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30}; // px size per cell
+    int cellSizeIdx = 3;
+    Vector2i gridSize = {windowSize.x/cellSizes[cellSizeIdx], windowSize.y/cellSizes[cellSizeIdx]};
     bool screenWrapping = true; // toroidal wrapping
 
     RenderWindow window(VideoMode(windowSize.x+1, windowSize.y+1), "Conroy's Game of Life", Style::Titlebar | Style::Close);
     vector<vector<cellState>> cellStates(gridSize.x, vector<cellState>(gridSize.y, Dead));
 
     RectangleShape cell{};
-    cell.setSize(Vector2f(cellSize-1, cellSize-1));
+    cell.setSize(Vector2f(cellSizes[cellSizeIdx]-1, cellSizes[cellSizeIdx]-1));
     cell.setOutlineThickness(1);
     cell.setOutlineColor(Color(224, 224, 224));
 
@@ -38,10 +39,32 @@ int main()
                 screenWrapping = !screenWrapping;
             }
 
+            // change cell size
+            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::Up) {
+                if (cellSizeIdx+1 >= cellSizes.size()) continue;
+                cellSizeIdx += 1;
+                gridSize = {windowSize.x/cellSizes[cellSizeIdx], windowSize.y/cellSizes[cellSizeIdx]};
+
+                cellStates.resize(gridSize.x);
+                for (vector<cellState>& row: cellStates) {
+                    row.resize(gridSize.y, Dead);
+                }
+            }
+            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::Down) {
+                if (cellSizeIdx-1 < 0) continue;
+                cellSizeIdx -= 1;
+                gridSize = {windowSize.x/cellSizes[cellSizeIdx], windowSize.y/cellSizes[cellSizeIdx]};
+
+                cellStates.resize(gridSize.x);
+                for (vector<cellState>& row: cellStates) {
+                    row.resize(gridSize.y, Dead);
+                }
+            }
+
             // add cells
             else if (Mouse::isButtonPressed(Mouse::Left)) {
                 Vector2i mousePosition = Mouse::getPosition(window);
-                Vector2i cellClicked = {mousePosition.x / cellSize, mousePosition.y / cellSize};
+                Vector2i cellClicked = {mousePosition.x / cellSizes[cellSizeIdx], mousePosition.y / cellSizes[cellSizeIdx]};
                 if (cellClicked.x < 0 || cellClicked.x >= gridSize.x || cellClicked.y < 0 || cellClicked.y >= gridSize.y) continue;
                 cellStates[cellClicked.x][cellClicked.y] = Alive;
             }
@@ -49,7 +72,7 @@ int main()
             // delete cells
             else if (Mouse::isButtonPressed(Mouse::Right)) {
                 Vector2i mousePosition = Mouse::getPosition(window);
-                Vector2i cellClicked = {mousePosition.x / cellSize, mousePosition.y / cellSize};
+                Vector2i cellClicked = {mousePosition.x / cellSizes[cellSizeIdx], mousePosition.y / cellSizes[cellSizeIdx]};
                 if (cellClicked.x < 0 || cellClicked.x >= gridSize.x || cellClicked.y < 0 || cellClicked.y >= gridSize.y) continue;
                 cellStates[cellClicked.x][cellClicked.y] = Dead;
             }
@@ -81,7 +104,7 @@ int main()
                     chrono::milliseconds duration(10);
                     this_thread::sleep_for(duration);
                     const bool stateChange = updateCellStates(cellStates, screenWrapping); // update cell states for next cycle; check if board changed
-                    refreshScreen(cellStates, cell, cellSize, gridSize, window);
+                    refreshScreen(cellStates, cell, cellSizes[cellSizeIdx], gridSize, window);
                     if (!stateChange) { // stop if nothing changed this iteration
                         break;
                     }
@@ -90,7 +113,7 @@ int main()
             }
         }
 
-        refreshScreen(cellStates, cell, cellSize, gridSize, window);
+        refreshScreen(cellStates, cell, cellSizes[cellSizeIdx], gridSize, window);
     }
 
     return 0;
